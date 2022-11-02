64b040269166b98db58876f350fa8da5fd620e24
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-02 15:34:06 -0300

    CSS

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,24 @@
 <script>
   import Navbar from './Navbar.svelte';
   import Title from './Title.svelte';
+  import Example from './Example.svelte';
 </script>
 
 <!-- <style></style> -->
 <!-- CSS/STYLING -->
 
-<!-- "props" shorhand for properties makes components dynamic set from outside -->
-
 <Navbar />
 <Title title="add expense" />
 <Title title="expense list" />
 <Title />
+<Example />
+<span>hello from app</span>
+
+<style>
+  /* no name collisions */
+  /* css bundle gestiona las clases añadiendo id únicos */
+  span {
+    background: green;
+    color: blue;
+  }
+</style>
---
+++ src/Example.svelte
@@ -0,0 +1,18 @@
+<span>
+  hello from example
+
+  <div class="main-title">
+    <h2>example title</h2>
+  </div>
+</span>
+
+<style>
+  /* fijar span a estilos globales, debes comentar los estilos del otro span */
+  :global(span) {
+    background: var(--primaryColor);
+    color: white;
+  }
+  .main-title {
+    color: red;
+  }
+</style>
