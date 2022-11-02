cab28eba2a84eabddcbd8d48ade25bb2c45fd557
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-02 14:29:12 -0300

    Navbar Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,8 @@
-<!-- <script></script> -->
-<!-- JAVASCRIPT/LOGIC -->
+<script>
+  import Navbar from './Navbar.svelte';
+</script>
 
 <!-- <style></style> -->
 <!-- CSS/STYLING -->
 
-<!-- HTML -->
-<nav class="nav">
-  <div class="nav-center">
-    <h1 class="nav-title">budget calculator</h1>
-    <button type="button" class="nav-btn">
-      <i class="far fa-plus-square" />
-      add item
-    </button>
-  </div>
-</nav>
+<Navbar />
