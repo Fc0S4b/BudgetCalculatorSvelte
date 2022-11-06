54b177e080c8fce73233c3f759796c99e3ad8849
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-06 02:03:14 -0300

    Expenses Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,14 @@
 <script>
+  // components
   import Navbar from './Navbar.svelte';
+  import ExpensesList from './ExpensesList.svelte';
+  // data
   import expensesData from './expenses';
+  // variables
   let expenses = [...expensesData];
-  console.log(expenses);
 </script>
 
 <Navbar />
+<main class="content">
+  <ExpensesList {expenses} />
+</main>
