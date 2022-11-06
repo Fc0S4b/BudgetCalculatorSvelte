51e6bfd3a8916166d6b336c10bc7b62a5747a489
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-05 23:45:20 -0300

    Expenses Data

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script>
   import Navbar from './Navbar.svelte';
-  import Title from './Title.svelte';
-  let fruits = ['apple', 'orange', 'lemon'];
+  import expensesData from './expenses';
+  let expenses = [...expensesData];
+  console.log(expenses);
 </script>
 
 <Navbar />
-
-{#each fruits as item}
-  <!-- <h1>fruit: {item}</h1> -->
-  <Title title={item} />
-{/each}
