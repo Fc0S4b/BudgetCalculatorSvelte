a9939390841ee46936c08c251af5ebb4c3bd3f1a
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-07 23:11:21 -0300

    Expense component

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -8,9 +8,22 @@
   export let amount = 0;
 </script>
 
-<div>
-  <!-- <p>{index}</p> -->
-  <!-- puede ser expense.name o expense.amount, pero mejor destructurado -->
-  <h1>title : {name}</h1>
-  <h1>amount : {amount}</h1>
-</div>
+<article class="single-expense">
+  <div class="expense-info">
+    <h2>
+      {name}
+      <button class="amount-btn">
+        <i class="fas fa-caret-down" />
+      </button>
+    </h2>
+    <h4>amount : ${amount}</h4>
+  </div>
+  <div class="expense-buttons">
+    <button class="expense-btn edit-btn">
+      <i class="fas fa-pen" />
+    </button>
+    <button class="expense-btn delete-btn">
+      <i class="fas fa-trash" />
+    </button>
+  </div>
+</article>
