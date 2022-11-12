fb5ca8ab31cc79be4e45458c519310559e5dcf2c
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-11 23:33:28 -0300

    Reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,18 @@
 <script>
   import { setContext } from 'svelte';
-
+  // components
   import Navbar from './Navbar.svelte';
   import ExpensesList from './ExpensesList.svelte';
+  import Totals from './Totals.svelte';
   // data
   import expensesData from './expenses';
-  import Expense from './Expense.svelte';
+
   // variables
   let expenses = [...expensesData];
+  // reactive
+  $: total = expenses.reduce((acc, curr) => {
+    return (acc += curr.amount);
+  }, 0);
   // functions
   function removeExpense(id) {
     expenses = expenses.filter((item) => item.id !== id);
@@ -21,6 +26,7 @@
 
 <Navbar />
 <main class="content">
+  <Totals title="total expenses" {total} />
   <ExpensesList {expenses} />
   <button
     type="button"
