f67486c947f06917b2e0fa8310fec7d58e1c4cfd
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-13 17:35:57 -0300

    form submission

--- src/ExpenseForm.svelte
+++ src/ExpenseForm.svelte
@@ -6,11 +6,16 @@
   // a truthy value is considered true when encountered in boolean context
 
   $: isEmpty = !name || !amount;
+  function handleSubmit() {
+    console.log({ name, amount });
+    name = '';
+    amount = null;
+  }
 </script>
 
 <section class="form">
   <Title title="add expense" />
-  <form class="expense-form">
+  <form class="expense-form" on:submit|preventDefault={handleSubmit}>
     <div class="form-control">
       <label for="name">name</label>
       <input type="text" id="name" bind:value={name} />
