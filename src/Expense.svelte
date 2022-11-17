81a0fc57c597a070f6d4c6697f87364f2f0a56d1
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-16 23:50:09 -0300

    Transition Basics

--- src/Expense.svelte
+++ src/Expense.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { blur, slide, scale, fade, fly } from 'svelte/transition';
   import { getContext } from 'svelte';
   export let id;
   export let name = '';
@@ -20,7 +21,11 @@
       </button>
     </h2>
     {#if displayAmount}
-      <h4>amount : ${amount}</h4>
+      <!-- <h4 transition:blur>amount : ${amount}</h4> -->
+      <!-- <h4 transition:scale>amount : ${amount}</h4> -->
+      <!-- <h4 transition:slide>amount : ${amount}</h4> -->
+      <!-- <h4 transition:fade>amount : ${amount}</h4> -->
+      <h4 transition:fly>amount : ${amount}</h4>
     {/if}
   </div>
   <div class="expense-buttons">
