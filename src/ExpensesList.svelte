fb3fab3509ea2ec2bc070dd5341bca6c0c12dc29
Fc0S4b <fcoj.sabugo@gmail.com>
2022-11-18 15:02:15 -0300

    Key Expression in Each Block and animate

--- src/ExpensesList.svelte
+++ src/ExpensesList.svelte
@@ -2,14 +2,19 @@
   import SectionTitle from './Title.svelte';
   import Expense from './Expense.svelte';
   import { fly } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
   export let expenses = [];
 </script>
 
 <section>
   <SectionTitle title="expense list" />
   <ul>
-    {#each expenses as expense, index}
-      <div transition:fly={{ x: 200, delay: (index + 1) * 700 }}>
+    {#each expenses as expense, index (expense.id)}
+      <div
+        in:fly={{ x: 200, delay: (index + 1) * 700 }}
+        out:fly={{ x: -200 }}
+        animate:flip
+      >
         <Expense {...expense} />
       </div>
     {:else}
